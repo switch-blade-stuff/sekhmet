ecca4a4bc0032742ccfaa2f6f59665c648cab26e
switchblade <terekhin.mikhail.01@gmail.com>
2022-05-18 12:27:58 -0700

    Updated docs

--- sekhmet/detail/object.hpp
+++ sekhmet/detail/object.hpp
@@ -14,7 +14,7 @@ namespace sek
 	template<typename T>
 	concept object_type = std::derived_from<std::remove_cv_t<T>, object>;
 	template<typename To, typename From>
-	concept valid_object_cast = object_type<From> && object_type<To> && std::same_as<std::remove_cv_t<To>, To>;
+	concept valid_object_cast = object_type<From> && object_type<To>;
 	// clang-format on
 
 	/** @brief Exception thrown when an object cast is not possible. */
@@ -34,22 +34,20 @@ namespace sek
 		[[nodiscard]] virtual type_info type_of() const noexcept = 0;
 	};
 
-	/** Casts between object types. */
+	/** Casts the `from` pointer to `ToPtr`. Supports up-casts and cross-casts.
+	 * @tparam ToPtr Pointer type to cast `from` to.
+	 * @param from Pointer to the object being cast.
+	 * @return `from` cast to `ToPtr` or nullptr if such cast is not possible.
+	 * @note Both types must be object types. */
 	template<typename ToPtr, typename From, typename To = std::remove_cv_t<std::remove_pointer_t<ToPtr>>>
-	constexpr ToPtr object_cast(From *from) noexcept
+	[[nodiscard]] constexpr ToPtr object_cast(From *from) noexcept
 		requires std::is_pointer_v<ToPtr> && valid_object_cast<To, From>
 	{
-		using Obj = transfer_cv_t<From, object>;
-
-		/* Object cast can do the following:
-		 * 1. `To` is base of `From` -> static_cast
-		 * 2. `From` is base of `To` -> Check that an upcast is possible, then cast through `basic_object`.
-		 * Otherwise, return nullptr, since types do not have anything in common. */
 		if constexpr (std::is_base_of_v<To, From>)
 			return static_cast<ToPtr>(from);
 		else if constexpr (std::is_base_of_v<From, To>)
 		{
-			auto *obj = static_cast<Obj *>(from);
+			auto *obj = static_cast<transfer_cv_t<From, object> *>(from);
 
 			/* Check that the actual type of `from` is the same as or a child of `To`. */
 			const auto to_type = type_info::get<To>();
@@ -59,10 +57,14 @@ namespace sek
 		}
 		return nullptr;
 	}
-	/** @copydoc object_cast
-	 * @throw bad_object_cast If a cast is not possible. */
+	/** Casts the `from` reference to `ToRef`. Supports up-casts and cross-casts.
+	 * @tparam ToRef Reference type to cast `from` to.
+	 * @param from Reference to the object being cast.
+	 * @return `from` cast to `ToRef`.
+	 * @throw bad_object_cast If a cast is not possible.
+	 * @note Both types must be object types. */
 	template<typename ToRef, typename From, typename To = std::remove_cv_t<std::remove_reference_t<ToRef>>>
-	constexpr ToRef &object_cast(From &from)
+	[[nodiscard]] constexpr ToRef &object_cast(From &from)
 		requires std::is_reference_v<ToRef> && valid_object_cast<To, From>
 	{
 		auto *result = object_cast<std::remove_reference_t<ToRef> *>(std::addressof(from));
