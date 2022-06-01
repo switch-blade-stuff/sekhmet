f6852134493250f3792710c97750e49d86ae99e9
switchblade <terekhin.mikhail.01@gmail.com>
2022-06-01 02:27:52 -0700

    Updated license

--- sekhmet/detail/meta_util.hpp
+++ sekhmet/detail/meta_util.hpp
@@ -1,6 +1,24 @@
-//
-// Created by switchblade on 2021-10-21.
-//
+/*
+ * ============================================================================
+ * Sekhmet - C++20 game engine & editor
+ * Copyright (C) 2022 switchblade
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ============================================================================
+ *
+ * Created by switchblade on 2021-10-21
+ */
 
 #pragma once
 
